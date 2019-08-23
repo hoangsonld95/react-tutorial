02dbacacb4a0679ca6778cd97f1ce682351db2a0
= <ledinhhoangson315@gmail.com>
2019-08-23 13:13:23 +0900

    Add delete functionality to each row of table

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from './Table.js'
 
 class App extends Component {
+
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
+  }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
   render() {
+    const { characters } = this.state
     return(
-      <div className="App">
-        <h1>Hello, React!</h1>
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter}/>
       </div>
     )
   }
